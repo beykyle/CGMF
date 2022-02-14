4da4433b20ab471b599a70930a979a2dcb243dfa
beykyle <beykyle@umich.edu>
2022-02-14 10:28:00 -0500

    Set GKD OMPs

--- libcgmf/src/omsetparm.cpp
+++ libcgmf/src/omsetparm.cpp
@@ -70,32 +70,50 @@ unsigned int omSetOmp(double e, ZAnumber* target, Pdata* proj, Optical *omp)
   const auto z1 = proj->particle.getZ();
   const auto a1 = proj->particle.getA();
   omInitOmp(omp);
+  unsigned int potfm0 = potfm;
 
   double a3 = pow((double)a0,1.0/3.0);
+  
+  if(auto gkd = proj->gkd) {
+    potfm = omp_library(potfm>>8, z0, a0, z1, a1, e, omp);
+    /***  Energy dependent depths */
+    omp->volume.real     = gkd->real_central_depth(z0,a0,e);
+    omp->surface.real    = 0;
+    omp->volume.imag     = gkd->compl_central_depth(z0,a0,e);
+    omp->surface.imag    = gkd->compl_surf_depth(z0,a0,e);
+    omp->spin_orbit.real = gkd->real_so_depth(z0,a0,e);
+    omp->spin_orbit.imag = gkd->compl_so_depth(z0,a0,e);
+    
+    /***  Reduced parameters */
+    omp->R0   = gkd->real_central_depth(z0,a0,e)*a3;
+    omp->R0s  = 0;
+    omp->Rv   = gkd->compl_central_depth(z0,a0,e)*a3;
+    omp->Rs   = gkd->compl_surf_depth(z0,a0,e)*a3;
+    omp->Rvso = gkd->real_so_depth(z0,a0,e)*a3;
+    omp->Rwso = gkd->compl_so_depth(z0,a0,e)*a3;
+    omp->Rc   = 0;
+    
+    omp->a0   = gkd->real_diffusivity(z0,a0,e);
+    omp->av   = omp->a0;
+    omp->avso = gkd->real_so_depth(z0,a0,e);
+    omp->awso = omp->avso;
+    omp->as   = gkd->compl_surf_diffusivity(z0,a0,e);
 
-  /***  Retrieve potential parameters from library */
-  unsigned int potfm0 = potfm;
-  potfm = omp_library(potfm>>8, z0, a0, z1, a1, e, omp);
+    potfm = 0x0013;
+  
+  } else  {
+    /***  Retrieve potential parameters from library */
+    potfm = omp_library(potfm>>8, z0, a0, z1, a1, e, omp);
 
-  /***  Energy dependent depths */
-  omp->volume.real     = omp->v1  + omp->v2  *e + omp->v3  *e*e;
-  omp->surface.real    = omp->vs1 + omp->vs2 *e + omp->vs3 *e*e;
-  omp->volume.imag     = omp->wv1 + omp->wv2 *e + omp->wv3 *e*e;
-  omp->surface.imag    = omp->ws1 + omp->ws2 *e + omp->ws3 *e*e;
-  omp->spin_orbit.real = omp->vso1+ omp->vso2*e + omp->vso3*e*e;
-  omp->spin_orbit.imag = omp->wso1+ omp->wso2*e + omp->wso3*e*e;
-
-  if(omp->volume.real   <= 0.0){
-    potfm=(potfm & 0x009f);      omp->volume.real    =0.0;
+    /***  Energy dependent depths */
+    omp->volume.real     = omp->v1  + omp->v2  *e + omp->v3  *e*e;
+    omp->surface.real    = omp->vs1 + omp->vs2 *e + omp->vs3 *e*e;
+    omp->volume.imag     = omp->wv1 + omp->wv2 *e + omp->wv3 *e*e;
+    omp->surface.imag    = omp->ws1 + omp->ws2 *e + omp->ws3 *e*e;
+    omp->spin_orbit.real = omp->vso1+ omp->vso2*e + omp->vso3*e*e;
+    omp->spin_orbit.imag = omp->wso1+ omp->wso2*e + omp->wso3*e*e;   
   }
-  if(omp->volume.imag   <= 0.0){
-    potfm=(potfm & 0x00fe);      omp->volume.imag    =0.0;
-  }
-  if(omp->surface.imag  <= 0.0){
-    potfm=(potfm & 0x00f9);      omp->surface.imag   =0.0;
-  }
-  if(omp->spin_orbit.real<= 0.0)   omp->spin_orbit.real=0.0;
-
+  
   /***  Reduced parameters */
   omp->R0   = omp->r0  *a3;
   omp->R0s  = omp->r0s *a3;
@@ -104,6 +122,23 @@ unsigned int omSetOmp(double e, ZAnumber* target, Pdata* proj, Optical *omp)
   omp->Rvso = omp->rvso*a3;
   omp->Rwso = omp->rwso*a3;
   omp->Rc   = omp->rc  *a3;
+
+
+  if(omp->volume.real   <= 0.0) {
+    potfm=(potfm & 0x009f);      
+    omp->volume.real  = 0.0;
+  }
+  if(omp->volume.imag   <= 0.0) {
+    potfm=(potfm & 0x00fe);      
+    omp->volume.imag = 0.0;
+  }
+  if(omp->surface.imag  <= 0.0) {
+    potfm=(potfm & 0x00f9);      
+    omp->surface.imag = 0.0;
+  }
+  if(omp->spin_orbit.real<= 0.0) {   
+    omp->spin_orbit.real=0.0;
+  }
 
   return( (potfm0 & 0xff00) | potfm );
 }
