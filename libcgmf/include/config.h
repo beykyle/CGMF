77b732485976929726f88f2a2efc8871634b95d1
Kyle Andrew Beyer <beykyle@armis20206.arc-ts.umich.edu>
2022-06-30 16:26:19 -0400

    random seed by time

--- libcgmf/include/config.h
+++ libcgmf/include/config.h
@@ -119,7 +119,7 @@ const bool INCLUDE_INTERNAL_CONVERSION = true;
  2: output MC history in the FFD input format
  */
 
-const bool RANDOM_SEED_BY_TIME = false;
+const bool RANDOM_SEED_BY_TIME = true;
 const bool PERTURB_EXCITATON_ENERGY = true;
 const int  EVENT_OUTPUT_FORMAT = 0;
 
