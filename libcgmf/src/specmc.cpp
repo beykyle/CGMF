542203d1801e7e56f08a91ef4ac5c3a862c19d3b
beykyle <beykyle@umich.edu>
2022-10-24 22:15:36 -0600

    Add back print mssg

--- libcgmf/src/specmc.cpp
+++ libcgmf/src/specmc.cpp
@@ -85,26 +85,25 @@ void setPdataOMP(string fname, int ip) {
 
   auto print_if_rank0 = [&ip,&fname](string OM_name) {
     if (ip ==0) {
-      printf(
-          string{"Reading "} + OM_name + " + parameters from " + fname);
+      printf("Reading %s parameters from %s", OM_name.c_str(), fname.c_str() );
     }
   };
 
   if ( fname.find("KD",start) != string::npos
       or fname.find("kd",start) != string::npos) {
-    print_if_rank0("KoningDelaroche03")
+    print_if_rank0("KoningDelaroche03");
     pdt[neutron].omp_file = new omplib::KoningDelaroche03<n>(j);
     pdt[neutron].omp = 6 << 8;
   }
   else if ( fname.find("CH", start) != string::npos
       or fname.find("ch", start) != string::npos) {
-    print_if_rank0("ChapelHill89")
+    print_if_rank0("ChapelHill89");
     pdt[neutron].omp_file = new omplib::ChapelHill89<n>(j);
     pdt[neutron].omp = 5 << 8;
   }
   else if ( fname.find("WLH", start) != string::npos
       or fname.find("wlh", start) != string::npos) {
-    print_if_rank0("WLH21")
+    print_if_rank0("WLH21");
     pdt[neutron].omp_file = new omplib::WLH21<n>(j);
     pdt[neutron].omp = 6 << 8;
   }
