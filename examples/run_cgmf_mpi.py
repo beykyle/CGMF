10f1da34204c014eb1372f0eeef52ed82c5c9356
Kyle Beyer <beykyle@umich.edu>
2023-04-07 12:23:47 -0600

    refactor MPI example

--- examples/run_cgmf_mpi.py
+++ examples/run_cgmf_mpi.py
@@ -9,25 +9,19 @@ from CGMFtk.histories import Histories
 import numpy as np
 from mpi4py import MPI
 
-def run_cgmf_mpi(inp : CGMF_Input):
-
-    comm = MPI.COMM_WORLD
-    size = comm.Get_size()
-    rank = comm.Get_rank()
-
-    inp.MPI_rank = rank
+def run_cgmf_mpi(inp : CGMF_Input, comm):
 
     # run worker
-    hists = run(cgmf_input)
+    hists = run(inp)
 
     # write results from just this rank:
-    hist_arr.save("histories_rank_{}.npy".format(rank))
+    hists.save("histories_rank_{}.npy".format(inp.MPI_rank))
 
     # gather histories from all MPI ranks
-    result = comm.gather(hist_arr.histories, root=0)
+    result = comm.gather(hists.histories, root=0)
 
     # concatenate them and print the result
-    if rank == 0:
+    if inp.MPI_rank == 0:
         # create new Histories object with history info from all MPI ranks,
         # write the output to disk as binary np array
         all_histories = Histories(  from_arr=np.concatenate( result, axis=0  ) )
@@ -36,7 +30,7 @@ def run_cgmf_mpi(inp : CGMF_Input):
 
     return None
 
-def test():
+def test(inp : CGMF_Input, size : int, all_histories : Histories):
     for i in range(size):
 
         # just check columns 0-4: A, Z, J, P, U
@@ -47,30 +41,39 @@ def test():
         # MPI gather respects the ordering of the ranks, so we know
         # exactly what indices each rank's results will be in after
         # concatenation
-        ibot = i * cgmf_input.nevents*2
-        itop = ibot + cgmf_input.nevents*2
+        ibot = i * inp.nevents*2
+        itop = ibot + inp.nevents*2
         gathered =np.array(
             all_histories.histories[ibot:itop,0:5],
             dtype="float")
 
         # comparison
-        assert( np.allclose(test, gathered))
+        assert(np.allclose(test, gathered))
 
 def main():
+
+    comm = MPI.COMM_WORLD
+    size = comm.Get_size()
+    rank = comm.Get_rank()
+
+
     # 100 events per MPI rank
-    cgmf_input = CGMF_Input(
+    inp = CGMF_Input(
         nevents  = 100,
         zaid     = 98252,
         einc     = 0.0
     )
 
+    inp.MPI_rank = rank
+
     # run on all workers
-    # result will only be meaningful on rank 0
-    result = run_cgmf_mpi(inp)
+    result = run_cgmf_mpi(inp, comm)
 
     if rank == 0:
+        assert(result is not None) # should retun Histories instance on rank 0
+        test(inp, size, result)
+        print("Test passed!")
         print("nubar: {}".format(result.nubartot()))
-        test()
 
 if __name__ == "__main__":
     main()
